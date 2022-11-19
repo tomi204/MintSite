f88f2c9ba40b39db1bc1882061dee29d1ead15b5
tomi204 <tomi204@outlook.com>
2022-11-19 08:45:21 -0300

    input para elegir la cantidad

--- components/mint/Mint.tsx
+++ components/mint/Mint.tsx
@@ -1,9 +1,16 @@
 import { ethers } from "ethers";
 import React from "react";
+import { useState } from "react";
 import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
 export const Mint = () => {
+  const [number, setNumber] = useState(1);
+  const number1 = ethers?.BigNumber.from(number);
+  const onChangeValue = (e) => {
+    setNumber((number) => parseInt(e.target.value));
+  };
+
   const { isConnected } = useAccount();
-  const number = ethers.BigNumber.from(1);
+  //const number = ethers.BigNumber.from(1);
   const { config } = usePrepareContractWrite({
     address: "0xC1C84F632a93cc4487bB2fbB6921DB47062f17c1",
     functionName: "mintNFT",
@@ -25,29 +32,32 @@ export const Mint = () => {
         type: "function",
       },
     ],
-    args: [number],
+    args: [number1],
   });
   const { write } = useContractWrite(config);
 
   return (
     <div>
       {isConnected && (
-        <button
-          style={{
-            backgroundColor: "#0d76fc",
-            color: "white",
-            paddingInline: "35px",
-            paddingBlock: "10px",
-            cursor: "pointer",
-            borderRadius: "20px",
-            border: "none",
-            fontSize: "15px",
-          }}
-          disabled={!write}
-          onClick={() => write?.()}
-        >
-          MINT
-        </button>
+        <div>
+          <button
+            style={{
+              backgroundColor: "#0d76fc",
+              color: "white",
+              paddingInline: "35px",
+              paddingBlock: "10px",
+              cursor: "pointer",
+              borderRadius: "20px",
+              border: "none",
+              fontSize: "15px",
+            }}
+            disabled={!write}
+            onClick={() => write?.()}
+          >
+            MINT
+          </button>
+          <input type="number" value={number} onChange={onChangeValue} />
+        </div>
       )}
     </div>
   );
